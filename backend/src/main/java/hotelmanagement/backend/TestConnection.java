cfbef2cd0653f785873692685b3baeff7706a2dd
UHUUITINHT <huutinhpc.2023@gmail.com>
2026-03-13 01:20:44 +0700

    KetNoi thanh cong database

---
+++ backend/src/main/java/hotelmanagement/backend/TestConnection.java
@@ -0,0 +1,15 @@
+package hotelmanagement.backend;
+import java.sql.Connection;
+
+
+public class TestConnection {
+    public static void main(String[] args) {
+        Connection conn = DBConnection.getConnection();
+
+        if (conn != null) {
+            System.out.println("Connected to database!");
+        } else {
+            System.out.println("Connection failed!");
+        }
+    }
+}
