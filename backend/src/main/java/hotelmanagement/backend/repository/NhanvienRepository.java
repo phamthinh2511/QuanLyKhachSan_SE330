0cc2cb8c621a65934c6236590224da0f5c258c51
NGUYENHUUTINH-uit <huutinhpc.2023@gmail.com>
2026-04-18 23:58:34 +0700

    feat: implement check-in logic and repositories

---
+++ backend/src/main/java/hotelmanagement/backend/repository/NhanvienRepository.java
@@ -0,0 +1,9 @@
+package hotelmanagement.backend.repository;
+
+import hotelmanagement.backend.entity.Nhanvien;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NhanvienRepository extends JpaRepository<Nhanvien, Integer> {
+}
